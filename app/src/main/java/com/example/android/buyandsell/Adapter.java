613157b0cb5deb4ba8d80441e3233802752599fe
Anirudh33Iyengar <anirudh3iyengar@gmail.com>
2017-10-07 07:04:50 +0530

    Added few modules and classes

---
+++ app/src/main/java/com/example/android/buyandsell/Adapter.java
@@ -0,0 +1,9 @@
+package com.example.android.buyandsell;
+
+/**
+ * Created by Anirudh on 07-10-2017.
+ */
+
+public class Adapter {
+//connect to the database using this class
+}
---
+++ app/src/main/java/com/example/android/buyandsell/LoginandRegister.java
@@ -0,0 +1,8 @@
+package com.example.android.buyandsell;
+
+/**
+ * Created by Anirudh on 07-10-2017.
+ */
+
+public class LoginandRegister {
+}
